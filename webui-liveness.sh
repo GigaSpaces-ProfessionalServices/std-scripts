186f7afcb0ff2073743a483d7597388779f0947e
Jason Nerothin <jasonnerothin@gmail.com>
2017-12-26 14:45:13 -0600

    Update webui-liveness.sh

--- webui-liveness.sh
+++ webui-liveness.sh
@@ -5,7 +5,7 @@ readonly STATUS_LOG="/tmp/webui-status.$(date +%Y-%m-%d-%H-%M-%S).log"
 while [ 1 ]
 do
 
-UPORDOWN=$(netstat -ano | grep 8099)
+UPORDOWN=$(netstat -anop | grep 8099)
 NOW=$(date)
 
   echo "${NOW} ::: ${UPORDOWN}" >> ${STATUS_LOG}
